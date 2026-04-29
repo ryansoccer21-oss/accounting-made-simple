ae03350fd6a83f455e560f3c18523df286ce8681
ryansoccer21-oss <ryansoccer21@gmail.com>
2026-04-28 21:34:23 -0400

    Add sitemap and host to robots metadata

--- app/robots.js
+++ app/robots.js
@@ -1,8 +1,10 @@
 export default function robots() {
   return {
+    host: "https://accounting-made-simple.vercel.app",
     rules: {
       userAgent: "*",
       allow: "/"
-    }
+    },
+    sitemap: "https://accounting-made-simple.vercel.app/sitemap.xml"
   };
 }
